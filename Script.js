76c35a2bbdf382a8b515abb603828df1d94b0c88
bzabza <akkdlyapereliva@mail.ru>
2017-08-04 11:40:29 +0300

    add button 'hide'

--- Script.js
+++ Script.js
@@ -1,17 +1,31 @@
 /**
  * Created by ARTUR on 01.08.2017.
  */
-
-    document.querySelector('button').onclick = function character() {
-        let personName = document.getElementById('name').value;
-        let personAge = document.getElementById('age').value;
-        let person = [{name:personName, age:personAge}];
-        let ul = document.querySelector('.personName');
-        let newLi = document.createElement('li');
-        ul.appendChild (newLi);
-        newLi.appendChild(document.createTextNode(personName)); 
-        function qu() {
-            
-        }
-
+    document.querySelector('.personName').style.display = 'none';
+    document.querySelector('#hidePerson').onclick = function viewInfo() {
+    document.querySelector('.personName').style.display = 'block';
     };
+//**if (viewInfo() == true) {
+//    document.querySelector('#hidePerson').onclick = function hideInfo() {
+//       document.querySelector('.personName').style.display = 'none';
+//};
+
+
+for (let i = 0; i < 1; i++)document.querySelector('#btn').onclick = function character() {
+    let personName = document.querySelector('#name').value;
+    let personAge = document.querySelector('#age').value;
+    let person = [{name: personName, age: personAge}];
+    let ul = document.querySelector('.personName');
+
+    for (let i = 0; i < 1; i++) {
+        let newLi = document.createElement('li');
+        ul.appendChild(newLi);
+        newLi.appendChild(document.createTextNode(person[0].name));
+    }
+
+    document.querySelector('li').onclick = function nextCharacter() {
+        let newP = document.createElement('p');
+        ul.appendChild(newP);
+        newP.appendChild(document.createTextNode(person[0].age));
+    }
+}
